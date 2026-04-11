f091d78f78ec710c0f9625e8f3f16499da6c4852
HoJLter <hojlter.work@gmail.com>
2026-04-11 22:04:17 +0300

    Add: triangulation v1

--- include/core/Algorithms.h
+++ include/core/Algorithms.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <cmath>
 #include "SFML/Graphics.hpp"
 
 
@@ -23,4 +24,43 @@ bool isConvex(sf::VertexArray dots, int len) {
         }
 	}
     return true;
+}
+
+float calcLen(sf::Vector2f a, sf::Vector2f b) {
+    return sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2));
+}
+
+struct DiagonalData {
+    sf::VertexArray diaDots;
+    float lenSum = 0;
+};
+
+DiagonalData calcLossLenDia(sf::VertexArray dots, int len) {
+    DiagonalData dia;
+
+    struct diaInfo {
+        int i;
+        int j;
+        float len;
+    };
+    for (int i = 0; i < len; ++i) {
+        float min = calcLen(dots[i].position, dots[0].position);
+        diaInfo minDia;
+        for (int j = 0; j < len; ++j) {
+            bool isNeighbor = ((j == i + 1 % len) || (j == (i - 1 + len) % len));
+            if (isNeighbor) {
+                continue;
+            }
+            float curLen = calcLen(dots[i].position, dots[j].position);
+            if (min > curLen) {
+                min = curLen;
+                minDia = { i, j, curLen };
+            }
+            
+        }
+        dia.diaDots.append(dots[minDia.i]);
+        dia.diaDots.append(dots[minDia.j]);
+        dia.lenSum += minDia.len;
+    }
+    return dia;
 }
