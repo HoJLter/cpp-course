7058370c74e77e0e8718379a36b49b517ffa3b3e
HoJLter <hojlter.work@gmail.com>
2026-04-13 11:13:10 +0300

    Update: ResultScene

--- include/scenes/ResultScene.h
+++ include/scenes/ResultScene.h
@@ -1,9 +1,11 @@
 #include "scenes/Scene.h"
+#include "ui/Button.h"
 
 class ResultScene : public Scene {
 private:
 	sf::ConvexShape contour;
 	sf::VertexArray dots;
+	Button calculate;
 
 public:
 	ResultScene(sf::Vector2u windowSize, ISceneSwitcher& sceneSwitcher);
--- src/scenes/ResultScene.cpp
+++ src/scenes/ResultScene.cpp
@@ -4,7 +4,13 @@
 ResultScene::ResultScene(sf::Vector2u windowSize, ISceneSwitcher& ss) :
 	Scene(ss),
 	contour(ss.shared.contour),
-	dots(ss.shared.dots)
+	dots(ss.shared.dots),
+	calculate({ 50.f, 20.f }, 
+		{ static_cast<float>(windowSize.x) / 2.f, windowSize.y - 150.f }, 
+		"Calculate",
+		[this]() {
+			
+		})
 {
 
 	dots.setPrimitiveType(sf::LineStrip);
@@ -14,12 +20,21 @@ ResultScene::ResultScene(sf::Vector2u windowSize, ISceneSwitcher& ss) :
 	Log::debug("ResultScene created");
 }
 
-void ResultScene::handleEvent(const sf::Event& event) {
+void ResultScene::handleEvent(const sf::Event& event){
+	calculate.handleEvent(event);
 }
 
 void ResultScene::update(sf::RenderWindow& window) {
+	sf::Vector2i curPosInt = sf::Mouse::getPosition(window);
+	sf::Vector2f curPosFloat(
+		static_cast<float>(curPosInt.x),
+		static_cast<float>(curPosInt.y)
+	);
+
+	calculate.update(curPosFloat);
 }
 
 void ResultScene::render(sf::RenderWindow& window) {
 	window.draw(contour);
+	calculate.render(window);
 }
